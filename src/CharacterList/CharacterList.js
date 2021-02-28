6c2507cdb3b2c0be6d3be1663b1013310a2b21df
topdreg <apercival314@gmail.com>
2021-02-28 14:09:53 -0800

    refactored hooks being used into a custom hook and added .env file containing keys

--- src/CharacterList/CharacterList.js
+++ src/CharacterList/CharacterList.js
@@ -1,48 +1,19 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React from 'react';
 import {View, FlatList} from 'react-native';
 import CharacterRow from './components/CharacterRow';
-import cryptoJS from 'crypto-js';
-
-const API_KEY = '0a4ca65bd3f729f67781b3d87da4c85a';
-const private_key = 'a04def0fac3538c7c3622f6a82d82314b1785cd1';
+import useFetchMarvelCharacters from './hooks/use-fetch-marvel-characters';
 
 const CharacterList = (props) => {
-  const [offset, setOffset] = useState(0);
-  const [characters, setCharacters] = useState([]);
-  const [refreshing, setRefreshing] = useState(false);
-
-  const fetchMore = useCallback(() => setOffset(offset + 15), [offset]);
-
-  useEffect(() => {
-    const fetchAPI = async () => {
-      const ts = new Date().getTime();
-      const hash = cryptoJS.MD5(ts + private_key + API_KEY);
-      let response = await fetch(
-        `https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${API_KEY}&hash=${hash}&offset=${offset}&limit=15`,
-      );
-      response = await response.json();
-      if (response?.data?.results !== undefined) {
-        setCharacters((oldCharacters) => {
-          if (offset === 0) {
-            return response.data.results;
-          } else {
-            return [...oldCharacters, ...response.data.results];
-          }
-        });
-      }
-    };
-    fetchAPI();
-  }, [offset]);
+  const {
+    characters,
+    fetchMoreCharacters,
+    refreshing,
+    onRefresh,
+  } = useFetchMarvelCharacters();
 
   const renderItem = ({item: {name, thumbnail}}) => (
     <CharacterRow name={name} thumbnail={thumbnail} />
   );
-
-  const onRefresh = () => {
-    setRefreshing(true);
-    setOffset(0);
-    setRefreshing(false);
-  };
 
   return (
     <View>
@@ -51,7 +22,7 @@ const CharacterList = (props) => {
         renderItem={renderItem}
         keyExtractor={(item) => item.id.toString()}
         onEndReachedThreshold={1}
-        onEndReached={fetchMore}
+        onEndReached={fetchMoreCharacters}
         refreshing={refreshing}
         onRefresh={onRefresh}
       />
