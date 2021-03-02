7c453bcd6ec65f2e45b94c4b4be954416b7511f0
topdreg <apercival314@gmail.com>
2021-03-02 05:59:05 -0800

    added white backgrounds to components

--- src/screens/CharacterList/components/CharacterRow/styles.js
+++ src/screens/CharacterList/components/CharacterRow/styles.js
@@ -2,6 +2,7 @@ import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
   container: {
+    backgroundColor: 'white',
     width: '100%',
     height: 100,
     borderBottomWidth: 0.4,
--- src/screens/CharacterPage/styles.js
+++ src/screens/CharacterPage/styles.js
@@ -4,6 +4,7 @@ export default StyleSheet.create({
   container: {
     width: '100%',
     height: '100%',
+    backgroundColor: 'white',
   },
   scrollView: {
     alignItems: 'center',
